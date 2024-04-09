3f464f332636cb3749f8ce928d91f1baff7da765
Kingkaspev <kingsleychiboy22@gmail.com>
2024-04-09 22:46:17 +0100

    Swagger Service configuration

---
+++ ISWBlacklist.API/ISWBlacklist.Application/DTOs/Item/ItemCreationDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ISWBlacklist.Application.DTOs.Item
+{
+    public class ItemCreationDto
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+        [Required]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
--- ISWBlacklist.API/ISWBlacklist/Configurations/SwaggerExtension.cs
+++ ISWBlacklist.API/ISWBlacklist/Configurations/SwaggerExtension.cs
@@ -1,6 +1,43 @@
-﻿namespace ISWBlacklist.Configurations
+﻿using Microsoft.OpenApi.Models;
+
+namespace ISWBlacklist.Configurations
 {
-    public class SwaggerExtension
+    public static class SwaggerExtension
     {
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ISBWBlacklist",
+                    Version = "v1",
+                    Description = "ISBWBlacklist is a blacklisting service to be used to blacklist various categories of data."
+                });
+                config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Insert Token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "Jwt",
+                    Scheme = "Bearer"
+                });
+                config.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+        }
     }
 }
